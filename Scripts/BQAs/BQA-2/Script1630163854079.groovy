5704f2af70f59389a6741313b4672afd359c57c3
Triet Nguyen <trietnguyenk1661996@gmail.com>
2021-11-06 17:29:56 +1100

    sa

--- Scripts/BQAs/BQA-2/Script1630163854079.groovy
+++ Scripts/BQAs/BQA-2/Script1630163854079.groovy
@@ -18,4 +18,4 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 
-//code fix 2 3 4 5 s vwss dasda aaaaaaaaaaa 111 2222 333333333
+//code fix 2 3 4 5 s vwss dasda aaaaaaaaaaa 111 2222 333333333 qqw
