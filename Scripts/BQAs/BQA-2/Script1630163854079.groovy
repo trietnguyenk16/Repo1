5dbfb3533ba19add1444e3c5dd65ee753fcb67e4
Triet Nguyen <trietnguyenk1661996@gmail.com>
2021-11-06 14:57:45 +1100

    test

--- Scripts/BQAs/BQA-2/Script1630163854079.groovy
+++ Scripts/BQAs/BQA-2/Script1630163854079.groovy
@@ -17,4 +17,4 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-//code fix 2
+//code fix 2 3
